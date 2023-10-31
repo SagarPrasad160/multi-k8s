7c85b629ce727005e0b5074b84a0dbaef9fa251e
SagarPrasad160 <sagarprasad916@gmail.com>
2023-10-31 13:05:11 +0530

    test deploy

--- server/index.js
+++ server/index.js
@@ -17,7 +17,10 @@ const pgClient = new Pool({
   database: keys.pgDatabase,
   password: keys.pgPassword,
   port: keys.pgPort,
-  ssl: keys.pgSSL ? { rejectUnauthorized: false } : false,
+  ssl:
+    process.env.NODE_ENV !== "production"
+      ? false
+      : { rejectUnauthorized: false },
 });
 
 pgClient.on("connect", (client) => {
